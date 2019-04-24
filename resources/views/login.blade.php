291a7a868a092c179562fa4705711b758787a268
Jenny Chung <jennychung1@Jenny-Chung.local>
2019-04-24 12:37:18 -0700

    login page styling

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,5 +3,26 @@
 
 @section('main')
 
+<div class="login">
+<div class="row">
+    <div class="col-1"> </div>
+<div class="pageTitle col-4"> Welcome </div>
+<!-- <div class="col-1"> </div> -->
+<div class= "searchbody col-6">
+  <div class="col-1"> </div>
+
+<div class="subheadPink"> Email </div>
+<input type="text" placeholder="Email" class="col">
+
+<p style="margin-top: 10px;"> </p>
+
+<div class="subheadPink"> Password </div>
+<input type="text" placeholder="Password" class="col">
+
+<button type="submit" style="margin-top: 20px;" class="col col-lg-2">Submit</button>
+
+</div>
+</div>
+</div>
 
 @endsection
